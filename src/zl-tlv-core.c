3aea014353d79e5eb3ea81383d0d19f4116c413b
Leesoo Ahn <yisooan@fedoraproject.org>
2019-12-18 23:16:12 +0900

    zl-tlv-core: Add eight core TLV types

---
+++ src/zl-tlv-core.c
@@ -0,0 +1,216 @@
+/* zl-tlv-core.c
+ *
+ * Copyright 2019 Leesoo Ahn <yisooan@fedoraproject.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <string.h>
+
+#include <priv/zl-mem.h>
+#include <zl-tlv-core.h>
+
+zl_tlv_end_lldpdu_t *
+zl_tlv_end_lldpdu_new (void)
+{
+  zl_tlv_t *parent;
+  zl_tlv_end_lldpdu_t *new_tlv;
+
+  new_tlv = zl_calloc (1, sizeof(*new_tlv));
+  zl_ret_val_if (new_tlv == NULL, NULL);
+
+  parent = (zl_tlv_t *)new_tlv;
+  parent->type = TLV_END_OF_LLDPDU;
+  parent->length = 0;
+
+  return new_tlv;
+}
+
+zl_tlv_chassis_id_t *
+zl_tlv_chassis_id_new (uint8_t subtype, uint8_t *src, size_t nbytes)
+{
+  zl_tlv_t *parent;
+  zl_tlv_chassis_id_t *new_tlv;
+
+  zl_ret_val_if_fail (src != NULL, NULL);
+  zl_ret_val_if_fail (0 < nbytes && nbytes < 256, NULL);
+
+  new_tlv = zl_calloc (1, sizeof(*new_tlv));
+  zl_ret_val_if (new_tlv == NULL, NULL);
+
+  parent = (zl_tlv_t *)new_tlv;
+  parent->type = TLV_CHASSIS_ID;
+  parent->length = 1 + nbytes;
+
+  new_tlv->subtype = subtype;
+  new_tlv->value = zl_memdup (src, nbytes);
+
+  return new_tlv;
+}
+
+zl_tlv_port_id_t *
+zl_tlv_port_id_new (uint8_t subtype, uint8_t *src, size_t nbytes)
+{
+  zl_tlv_t *parent;
+  zl_tlv_port_id_t *new_tlv;
+
+  zl_ret_val_if_fail (src != NULL, NULL);
+  zl_ret_val_if_fail (0 < nbytes && nbytes < 256, NULL);
+
+  new_tlv = zl_calloc (1, sizeof(*new_tlv));
+  zl_ret_val_if (new_tlv == NULL, NULL);
+
+  parent = (zl_tlv_t *)new_tlv;
+  parent->type = TLV_PORT_ID;
+  parent->length = 1 + nbytes;
+
+  new_tlv->subtype = subtype;
+  new_tlv->value = zl_memdup (src, nbytes);
+
+  return new_tlv;
+}
+
+zl_tlv_ttl_t *
+zl_tlv_ttl_new (uint16_t src)
+{
+  zl_tlv_t *parent;
+  zl_tlv_ttl_t *new_tlv;
+
+  new_tlv = zl_calloc (1, sizeof(*new_tlv));
+  zl_ret_val_if (new_tlv == NULL, NULL);
+
+  parent = (zl_tlv_t *)new_tlv;
+  parent->type = TLV_TIME_TO_LIVE;
+  parent->length = 2;
+
+  new_tlv->value = src;
+
+  return new_tlv;
+}
+
+zl_tlv_port_desc_t *
+zl_tlv_port_desc_new (uint8_t *src, size_t nbytes)
+{
+  zl_tlv_t *parent;
+  zl_tlv_port_desc_t *new_tlv;
+
+  zl_ret_val_if_fail (src != NULL, NULL);
+  zl_ret_val_if_fail (0 < nbytes && nbytes < 256, NULL);
+
+  new_tlv = zl_calloc (1, sizeof(*new_tlv));
+  zl_ret_val_if (new_tlv == NULL, NULL);
+
+  parent = (zl_tlv_t *)new_tlv;
+  parent->type = TLV_PORT_DESCRIPTION;
+  parent->length = nbytes;
+
+  new_tlv->value = zl_memdup (src, nbytes);
+
+  return new_tlv;
+}
+
+zl_tlv_sys_name_t *
+zl_tlv_sys_name_new (uint8_t *src, size_t nbytes)
+{
+  zl_tlv_t *parent;
+  zl_tlv_sys_name_t *new_tlv;
+
+  zl_ret_val_if_fail (src != NULL, NULL);
+  zl_ret_val_if_fail (0 < nbytes && nbytes < 256, NULL);
+
+  new_tlv = zl_calloc (1, sizeof(*new_tlv));
+  zl_ret_val_if (new_tlv == NULL, NULL);
+
+  parent = (zl_tlv_t *)new_tlv;
+  parent->type = TLV_SYSTEM_NAME;
+  parent->length = nbytes;
+
+  new_tlv->value = zl_memdup (src, nbytes);
+
+  return new_tlv;
+}
+
+zl_tlv_sys_desc_t *
+zl_tlv_sys_desc_new (uint8_t *src, size_t nbytes)
+{
+  zl_tlv_t *parent;
+  zl_tlv_sys_desc_t *new_tlv;
+
+  zl_ret_val_if_fail (src != NULL, NULL);
+  zl_ret_val_if_fail (0 < nbytes && nbytes < 256, NULL);
+
+  new_tlv = zl_calloc (1, sizeof(*new_tlv));
+  zl_ret_val_if (new_tlv == NULL, NULL);
+
+  parent = (zl_tlv_t *)new_tlv;
+  parent->type = TLV_SYSTEM_DESCRIPTION;
+  parent->length = nbytes;
+
+  new_tlv->value = zl_memdup (src, nbytes);
+
+  return new_tlv;
+}
+
+zl_tlv_sys_cap_t *
+zl_tlv_sys_capabilities_new (uint16_t sys_caps, uint16_t enabled_caps)
+{
+  zl_tlv_t *parent;
+  zl_tlv_sys_cap_t *new_tlv;
+
+  new_tlv = zl_calloc (1, sizeof(*new_tlv));
+  zl_ret_val_if (new_tlv == NULL, NULL);
+
+  parent = (zl_tlv_t *)new_tlv;
+  parent->type = TLV_SYSTEM_CAPABILITIES;
+  parent->length = 4;
+
+  new_tlv->sys_caps = sys_caps;
+  new_tlv->enabled_caps = enabled_caps;
+
+  return new_tlv;
+}
+
+zl_tlv_mgmt_addr_t *
+zl_tlv_mgmt_addr_new (uint8_t mgmt_addr_len,
+                      uint8_t mgmt_addr_subtype,
+                      uint8_t *mgmt_addr_val,
+                      uint8_t iface_subtype,
+                      uint32_t iface_number,
+                      uint8_t oid_str_len,
+                      uint8_t *oid_str_val)
+{
+  zl_tlv_t *parent;
+  zl_tlv_mgmt_addr_t *new_tlv;
+
+  zl_ret_val_if_fail (mgmt_addr_val != NULL, NULL);
+  zl_ret_val_if_fail (oid_str_val != NULL, NULL);
+  zl_ret_val_if_fail (1 < mgmt_addr_len && mgmt_addr_len < 33, NULL);
+  zl_ret_val_if_fail (oid_str_len < 129, NULL);
+
+  new_tlv = zl_calloc (1, sizeof(*new_tlv));
+  zl_ret_val_if (new_tlv == NULL, NULL);
+
+  parent = (zl_tlv_t *)new_tlv;
+  parent->type = TLV_MANAGEMENT_ADDRESS;
+  parent->length = (1 + mgmt_addr_len + 1 + 4 + 1 + oid_str_len);
+
+  new_tlv->addr_len = mgmt_addr_len;
+  new_tlv->addr_subtype = mgmt_addr_subtype;
+  new_tlv->addr_value = zl_memdup (mgmt_addr_val, mgmt_addr_len - 1 /* Size of subtype */);
+  new_tlv->iface_subtype = iface_subtype;
+  new_tlv->iface_number = iface_number;
+  new_tlv->oid_len = oid_str_len;
+  new_tlv->oid_value = zl_memdup (oid_str_val, oid_str_len);
+
+  return new_tlv;
+}
---
+++ src/zl-tlv-core.h
@@ -0,0 +1,144 @@
+/* zl-tlv-core.h
+ *
+ * Copyright 2019 Leesoo Ahn <yisooan@fedoraproject.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __ZL_TLV_CORE_H__
+#define __ZL_TLV_CORE_H__
+
+#include <stdint.h>
+
+#include <priv/zl-macro.h>
+#include <zl-list.h>
+#include <zl-tlv.h>
+
+/* -- End Of LLDPDU (Mandatory) -- */
+typedef zl_tlv_t zl_tlv_end_lldpdu_t;
+
+/* -- Chassis ID (Mandatory) -- */
+typedef struct _zl_tlv_chassis_id_t zl_tlv_chassis_id_t;
+struct _zl_tlv_chassis_id_t
+{
+  zl_tlv_t  parent;
+
+  /* 0: Reserved
+   * 1: Chassis component
+   * 2: Interface alias
+   * 3: Port component
+   * 4: MAC address
+   * 5: Network address
+   * 6: Interface name
+   * 7: Locally assigned
+   * 8-255: Reserved
+   */
+  uint8_t   subtype;
+  uint8_t   *value;
+} zl_1byte_pack;
+
+/* -- Port ID (Mandatory) -- */
+typedef struct _zl_tlv_port_id_t zl_tlv_port_id_t;
+struct _zl_tlv_port_id_t
+{
+  zl_tlv_t  parent;
+
+  /* 0: Reserved
+   * 1: Interface alias
+   * 2: Port component
+   * 3: MAC address
+   * 4: Network address
+   * 5: Interface name
+   * 6: Agent circuit ID
+   * 7: Locally assigned
+   * 8-255: Reserved
+   */
+  uint8_t   subtype;
+  uint8_t   *value;
+} zl_1byte_pack;
+
+/* -- Time To Live (Mandatory) -- */
+typedef struct _zl_tlv_ttl_t zl_tlv_ttl_t;
+struct _zl_tlv_ttl_t
+{
+  zl_tlv_t  parent;
+
+  uint16_t  value;
+} zl_1byte_pack;
+
+/* -- Port Description (Optional) -- */
+typedef struct _zl_tlv_port_desc_t zl_tlv_port_desc_t;
+struct _zl_tlv_port_desc_t
+{
+  zl_tlv_t  parent;
+
+  uint8_t   *value;
+} zl_1byte_pack;
+
+/* -- System Name (Optional) -- */
+typedef struct _zl_tlv_sys_name_t zl_tlv_sys_name_t;
+struct _zl_tlv_sys_name_t
+{
+  zl_tlv_t  parent;
+
+  uint8_t   *value;
+} zl_1byte_pack;
+
+/* -- System Description (Optional) -- */
+typedef struct _zl_tlv_sys_desc_t zl_tlv_sys_desc_t;
+struct _zl_tlv_sys_desc_t
+{
+  zl_tlv_t  parent;
+
+  uint8_t   *value;
+} zl_1byte_pack;
+
+/* -- System Capabilities (Optional) -- */
+typedef struct _zl_tlv_sys_cap_t zl_tlv_sys_cap_t;
+struct _zl_tlv_sys_cap_t
+{
+  zl_tlv_t  parent;
+
+  /* .... .... .... ...X : Other
+   * .... .... .... ..X. : Repeater
+   * .... .... .... .X.. : Bridge
+   * .... .... .... X... : WLAN Access Point
+   * .... .... ...X .... : Router
+   * .... .... ..X. .... : Telephone
+   * .... .... .X.. .... : DOCSIS cable device
+   * .... .... X... .... : Station Only
+   * XXXX XXXX .... .... : Reserved */
+  uint16_t  sys_caps;
+  /* Functionalities of each bits are the same as system capabilities */
+  uint16_t  enabled_caps;
+} zl_1byte_pack;
+
+/* -- Management Address (Optional) -- */
+typedef struct _zl_tlv_mgmt_addr_t zl_tlv_mgmt_addr_t;
+struct _zl_tlv_mgmt_addr_t
+{
+  zl_tlv_t  parent;
+
+  uint8_t   addr_len;
+  /* Management Address String */
+  uint8_t   addr_subtype;
+  uint8_t   *addr_value;
+
+  uint8_t   iface_subtype;
+  uint32_t  iface_number;
+
+  uint8_t   oid_len;
+  uint8_t   *oid_value;
+} zl_1byte_pack;
+
+#endif
