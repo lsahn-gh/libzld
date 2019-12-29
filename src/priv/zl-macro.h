e9a6bd347c2fe58a4b5e87b52c9c1eeba90ff0b4
Leesoo Ahn <yisooan@fedoraproject.org>
2019-12-29 20:12:58 +0900

    macro: Add some useful macros
    - likely, and unlikely
    - zl_assert

--- src/priv/zl-macro.h
+++ src/priv/zl-macro.h
@@ -1,6 +1,8 @@
 #ifndef __ZL_MACRO_H__
 #define __ZL_MACRO_H__
 
+#include <assert.h>
+
 #include <zl-log.h>
 
 #define zl_ret_if(expr) \
@@ -35,5 +37,12 @@
 /* -- Platform Dependency Macros -- */
 #define zl_nbyte_pack(n) __attribute((aligned(n), packed))
 #define zl_1byte_pack zl_nbyte_pack(1)
+
+#define likely(x)    __builtin_expect((x),1)
+#define unlikely(x)  __builtin_expect((x),0)
+
+
+/* -- Assert Macros -- */
+#define zl_assert(expr) assert(expr)
 
 #endif
