5f2c118d4e8b4781df78c68d161d8658c7f5cd79
Leesoo Ahn <yisooan@fedoraproject.org>
2020-02-20 00:44:24 +0900

    priv/zl-macro: Add GET_VOID macro

--- src/priv/zl-macro.h
+++ src/priv/zl-macro.h
@@ -1,3 +1,20 @@
+/* zl-macro.h
+ *
+ * Copyright 2019-2020 Leesoo Ahn <yisooan@fedoraproject.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef __ZL_MACRO_H__
 #define __ZL_MACRO_H__
 
@@ -44,5 +61,10 @@
 
 /* -- Assert Macros -- */
 #define zl_assert(expr) assert(expr)
+
+
+/* -- Utils -- */
+#define GET_VOID(ptr) ((void *)str)
+
 
 #endif
