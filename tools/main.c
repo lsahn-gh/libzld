44c81e55a350eacb0ad4d16d53bc8ff083a26e15
Leesoo Ahn <yisooan@fedoraproject.org>
2020-02-20 23:08:10 +0900

    tools/main: use dlist based queue

--- tools/main.c
+++ tools/main.c
@@ -19,7 +19,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "zl-list.h"
+#include "zl-queue.h"
 #include "zl-object.h"
 #include "zl-tlv-type.h"
 #include "zl-tlv-core.h"
@@ -30,45 +30,43 @@ int
 main (int argc,
       char *argv[])
 {
-  zl_list_t * lldp;
+  zl_queue_t lldp = ZL_QUEUE_HEAD_INIT(lldp);
   void *ptr;
   char *str;
 
-  lldp = zl_list_new ();
-
   ptr = zl_tlv_chassis_id_new (4, macaddr, sizeof(macaddr));
-  zl_list_enqueue (lldp, ptr);
+  zl_queue_push (&lldp, ptr);
 
   str = "1/1";
   ptr = zl_tlv_port_id_new (5, GET_VOID(str), strlen(str));
-  zl_list_enqueue (lldp, ptr);
+  zl_queue_push (&lldp, ptr);
 
   ptr = zl_tlv_ttl_new (120);
-  zl_list_enqueue (lldp, ptr);
+  zl_queue_push (&lldp, ptr);
 
   str = "Summit300-48-Port 1001";
   ptr = zl_tlv_port_desc_new (GET_VOID(str), strlen(str) + 1/*NULL term*/);
-  zl_list_enqueue (lldp, ptr);
+  zl_queue_push (&lldp, ptr);
 
   str = "Summit300-48";
   ptr = zl_tlv_sys_name_new (GET_VOID(str), strlen(str) + 1);
-  zl_list_enqueue (lldp, ptr);
+  zl_queue_push (&lldp, ptr);
 
   str = "Summit300-48 - Version 7.4e.1 (Build 5) by Release_Master";
   ptr = zl_tlv_sys_desc_new (GET_VOID(str), strlen(str) + 1);
-  zl_list_enqueue (lldp, ptr);
+  zl_queue_push (&lldp, ptr);
 
   ptr = zl_tlv_sys_capabilities_new (0x0014, 0x0014);
-  zl_list_enqueue (lldp, ptr);
+  zl_queue_push (&lldp, ptr);
 
   uint8_t mgmt_addr[] = { 0x00, 0x01, 0x30, 0xf9, 0xad, 0xa0 };
   ptr = zl_tlv_mgmt_addr_new (7, 6, mgmt_addr, /* Management Address */
                               2, 1001, /* Interface */
                               0, NULL /* OID String */);
-  zl_list_enqueue (lldp, ptr);
+  zl_queue_push (&lldp, ptr);
 
   ptr = zl_tlv_end_lldpdu_new ();
-  zl_list_enqueue (lldp, ptr);
+  zl_queue_push (&lldp, ptr);
 
   return 0;
 }
